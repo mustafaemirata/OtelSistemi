682dc828bb3ba8af495c5602ad19d8672298daef
Mustafa Emir Ata <mustafaemir.ata@outlook.com>
2025-05-16 21:37:46 +0300

    Add project files.

---
+++ OtelSistemi/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OtelSistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void barButtonItem2_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Formlar.Tanımlamalar.FrmDurum fr=new Formlar.Tanımlamalar.FrmDurum();
+            fr.Show();  
+        }
+    }
+}
